c599d4afaad6a8a03646e047f36301f14d4885af
Sakae Watanabe <69649930+meltdownlv@users.noreply.github.com>
2021-03-18 13:19:43 -0700

    Update single line comments.

--- Student Roster/Student.h
+++ Student Roster/Student.h
@@ -18,11 +18,12 @@ private:
 public:
     // Default constructor
     Student();
-    // Full constructor with parameters
+    // Parameterized constructor
     Student(string student_id, string first_name, string last_name,
         string email_address, int age, int* days_to_complete, DegreeType degree);
+    // Default destructor
     ~Student();
-    // Accessor function for each class variable
+    // Declarations for all accessors
     string GetID();
     string GetFirstName();
     string GetLastName();
@@ -32,7 +33,7 @@ public:
     DegreeType GetDegreeProgram();
     int GetAvgDays();
 
-    // Mutator function for each class variable
+    // Declarations for all mutators
     void SetID(string new_id);
     void SetFirstName(string new_first_name);
     void SetLastName(string new_last_name);
@@ -40,7 +41,8 @@ public:
     void SetAge(int new_age);
     void SetDaystoComplete(int* new_course_days);
     void SetDegreeType(DegreeType new_degree_type);
-    void SetDegreeType(int new_degree_type); // In case program passed as int
+    void SetDegreeType(int new_degree_type);
+    
     void PrintAttributes();
 
 };
